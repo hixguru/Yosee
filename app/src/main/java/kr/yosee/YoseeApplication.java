8dcb20f5e564ee5ef26332c180f197bafbb299a3
hixguru <ikxguru@gmail.com>
2017-01-31 01:36:32 +0900

    Application Class 및 AppModule가

--- app/src/main/java/kr/yosee/YoseeApplication.java
+++ app/src/main/java/kr/yosee/YoseeApplication.java
@@ -1,8 +1,34 @@
 package kr.yosee;
 
+import android.app.Application;
+import android.content.Context;
+import kr.yosee.dagger.module.AppModule;
+import kr.yosee.dagger.view.AppComponent;
+import kr.yosee.dagger.view.DaggerAppComponent;
+
 /**
  * Created by hwanik on 2017. 1. 30..
  */
 
-public class YoseeApplication {
+public class YoseeApplication extends Application {
+    protected AppComponent component;
+
+    public static YoseeApplication get(Context context) {
+        return (YoseeApplication) context.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        component = DaggerAppComponent.builder()
+            .appModule(new AppModule(this))
+            .build();
+
+        component.inject(this);
+    }
+
+    public AppComponent getComponent() {
+        return component;
+    }
 }
